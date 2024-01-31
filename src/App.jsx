04d7bec7cb04be2b35edfa3486bd264482c17a95
Krishna Singh <krishnasingh781630@gmail.com>
2024-01-31 13:52:56 +0530

    routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-
-  return <div className="text-3xl text-green-700"> hi guys</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
