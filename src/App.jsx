3ba7d5ec284ab8239d13654c4785a0cda375367e
Krishna Singh <krishnasingh781630@gmail.com>
2024-01-31 23:02:13 +0530

    redux :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName } = user;
+        dispatch(addUser(uid, email, displayName  ));
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser())
+      }
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
